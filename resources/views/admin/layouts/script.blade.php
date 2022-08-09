e6199f4bed9e05821310ab8743ed670c0a8b9d8d
Van Hung Nguyen <hungnguyenpisces@gmail.com>
2022-08-09 13:23:01 +0700

    add libs

--- resources/views/admin/layouts/script.blade.php
+++ resources/views/admin/layouts/script.blade.php
@@ -1,11 +1,8 @@
 <!-- Bootstrap 5.2.0 js -->
 <script src="{!! url('assets/bootstrap/js/bootstrap.bundle.min.js') !!}"></script>
-<script src="{!! url('assets/bootstrap/js/bootstrap.esm.min.js') !!}"></script>
-<script src="{!! url('assets/bootstrap/js/bootstrap.min.js') !!}"></script>
 
 <!-- Font Awesome 6.1.2 js -->
 <script src="{!! url('assets/fontawesome/js/all.min.js') !!}"></script>
-<script src="{!! url('assets/fontawesome/js/brands.min.js') !!}"></script>
-<script src="{!! url('assets/fontawesome/js/fontawesome.min.js') !!}"></script>
-<script src="{!! url('assets/fontawesome/js/regular.min.js') !!}"></script>
-<script src="{!! url('assets/fontawesome/js/solid.min.js') !!}"></script>
+
+<!-- Custom js -->
+<script src="{!! url('assets/js/admin-scripts.js') !!}"></script>
