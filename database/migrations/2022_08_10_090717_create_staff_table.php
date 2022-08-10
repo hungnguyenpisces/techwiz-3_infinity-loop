3aecb9345d3dad91cb677587a80801d2fa05f09a
NguyenDuyTruongGiang <Bizuma123@gmail.com>
2022-08-10 18:40:28 +0700

    create db migration final

--- database/migrations/2022_08_10_090717_create_staff_table.php
+++ database/migrations/2022_08_10_090717_create_staff_table.php
@@ -13,20 +13,19 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('staff', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('gender');
+            $table->bigInteger('role');
             $table->string('date_of_birth');
-            $table->string('blood_group');
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('phone')->unique()->nullable();
             $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -38,6 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('staff');
     }
 };
