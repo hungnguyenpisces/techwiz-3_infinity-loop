f5ab842fe16093ddf833b3e131608a2099414980
NguyenDuyTruongGiang <Bizuma123@gmail.com>
2022-08-13 21:17:25 +0700

    asd

--- resources/views/user/layouts/script.blade.php
+++ resources/views/user/layouts/script.blade.php
@@ -3,7 +3,7 @@
 <script src="{!! url('assets/fontawesome/js/all.min.js') !!}"></script>
 <script src="{!! url('assets/js/wow.min.js') !!}"></script>
 <script src="{!! url('assets/js/tiny-slider.js') !!}"></script>
-<!-- <script src="{!! url('assets/js/glightbox.min.js') !!}"></script> -->
+<script src="{!! url('assets/js/glightbox.min.js') !!}"></script>
 <script src="{!! url('assets/js/count-up.min.js') !!}"></script>
 <!-- <script src="{!! url('assets/js/imagesloaded.min.js') !!}"></script> -->
 <!-- <script src="{!! url('assets/js/isotope.min.js') !!}"></script> -->
