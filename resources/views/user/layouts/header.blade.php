0d0d12df52caf2712a8e199d13aaadb89e956904
htnnth102 <96532248+htnnth102@users.noreply.github.com>
2022-08-13 08:41:07 +0700

    user styles

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -1,12 +1,44 @@
-<header>
-  <a href="#" class="logo">SAMEER SAINI</a>
-  <nav>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">Projects</a></li>
-      <li><a href="#">Blog</a></li>
-      <li><a href="#" class="cta">Contact</a></li>
-    </ul>
-  </nav>
+<header class="header navbar-area style2">
+
+  <div class="top-bar">
+    <div class="container">
+      <div class="inner-topbar">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-12">
+            <div class="top-contact">
+              <ul>
+                <li><i class="lni lni-envelope"></i><a href="https://demo.graygrids.com/cdn-cgi/l/email-protection#f69f989099b69b93929f91849f9285d895999b"><span class="__cf_email__" data-cfemail="f39a9d959cb39e96979a94819a9780dd909c9e">[email&#160;protected]</span></a></li>
+                <li><i class="lni lni-phone"></i> +099 254 254 9855</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-12">
+            <div class="right-content">
+              <div class="login-button">
+                <ul>
+                  <li>
+                    <a href="login.html"><i class="lni lni-enter"></i> Login</a>
+                  </li>
+                  <li>
+                    <a href="registration.html"><i class="lni lni-user"></i> Register</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="top-social">
+                <ul>
+                  <li><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
+                  </li>
+                  <li><a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a>
+                  </li>
+                  <li><a href="javascript:void(0)"><i class="lni lni-instagram"></i></a></li>
+                  <li><a href="javascript:void(0)"><i class="lni lni-youtube"></i></a></li>
+                  <li><a href="javascript:void(0)"><i class="lni lni-pinterest"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </header>
--- resources/views/user/layouts/sidebar.blade.php
+++ resources/views/user/layouts/sidebar.blade.php
@@ -10,7 +10,7 @@
                 <span class="profession">Ho va ten</span>
             </div>
         </div>
-        <span class="toggle"> <i class="fa-solid fa-arrow-right-from-bracket"></i></span>
+        <span class="toggle"> <i class="fa-solid fa-xl fa-arrow-right-from-bracket"></i></span>
     </header>
 
     <div class="menu-bar">
@@ -22,35 +22,35 @@
             <ul class="menu-links">
                 <li class="nav-link" id="user">
                     <a href="/user">
-                        <i class="fa-solid fa-user icon"></i>
+                        <i class="fa-solid  fa-user icon2"></i>
                         <span class="text nav-text">Dashboard</span>
                     </a>
                 </li>
 
                 <li class="nav-link" id="user-bmi">
                     <a href="/user-bmi">
-                        <i class="fa-solid fa-person icon"></i>
+                        <i class="fa-solid fa-xl fa-person icon2"></i>
                         <span class="text nav-text">Your BMI</span>
                     </a>
                 </li>
 
                 <li class="nav-link" id="user-chart">
                     <a href="/user-chart">
-                        <i class="fa-solid fa-chart-line icon"></i>
+                        <i class="fa-solid fa-chart-line icon2"></i>
                         <span class="text nav-text">Health charts</span>
                     </a>
                 </li>
 
                 <li class="nav-link" id="user-history">
                     <a href="/user-history">
-                        <i class="fa-regular fa-calendar-check icon"></i>
+                        <i class="fa-regular fa-calendar-check icon2"></i>
                         <span class="text nav-text">Appointments</span>
                     </a>
                 </li>
 
                 <li class="nav-link">
                     <a href="#">
-                        <i class="fa-solid fa-bars icon"></i>
+                        <i class="fa-solid fa-bars icon2"></i>
                         <span class="text nav-text">Setting</span>
                     </a>
                 </li>
@@ -61,18 +61,13 @@
         <div class="bottom-content">
             <li class="">
                 <a href="#">
-                    <i class="fa-solid fa-arrow-right-from-bracket icon"></i>
+                    <i class="fa-solid fa-arrow-right-from-bracket icon2"></i>
                     <span class="text nav-text">Logout</span>
                 </a>
             </li>
 
             <li class="mode">
-                <div class="sun-moon">
-                    <i class="fa-solid fa-moon icon sun"></i>
-                    <i class="fa-solid fa-moon icon moon"></i>
-                </div>
                 <span class="mode-text text">Dark mode</span>
-
                 <div class="toggle-switch">
                     <span class="switch"></span>
                 </div>
