68aa59883651207c48605bef6341411764bf1994
hungnguyenpisces <hungnguyenpisces@gmail.com>
2022-09-24 21:20:18 +0700

    delete constructor middle ware

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -9,93 +9,87 @@ use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
-    function __construct()
-    {
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\View\View
+   */
+  public function index(Request $request)
+  {
+    // $permissions = Permission::all();
+    $permissions = Permission::orderBy('id', 'DESC')->paginate(10);
+    return view('admin.permissions.index', compact('permissions'));
+  }
 
-        $this->middleware('role:Super-Admin|Admin');
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\View\View
+   */
+  public function create()
+  {
+    return redirect()->route('permissions.index');
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param \Illuminate\Http\Request $request
+   *
+   * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+   */
+  public function store(Request $request)
+  {
+
+    $this->validate($request, [
+      'name' => 'required|unique:permissions',
+    ]);
+
+    if (Auth::user()->hasRole('Super-Admin')) {
+      $permission = Permission::create(['name' => $request->input('name')]);
+      $permission->assignRole('Super-Admin');
+      $request->session()->flash('success', 'Permission created successfully');
+      return redirect()->route('permissions.index');
+    } else {
+      $request->session()->flash('error', 'You do not have permission to create permission');
+      return redirect()->route('permissions.index');
     }
+    return redirect('/403');
+  }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function index(Request $request)
-    {
-        // $permissions = Permission::all();
-        $permissions = Permission::orderBy('id', 'DESC')->paginate(10);
-        return view('admin.permissions.index', compact('permissions'));
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   *
+   * @return \Illuminate\View\View
+   */
+  public function show($id)
+  {
+    return redirect()->route('permissions.index');
+  }
+
+  public function edit($id)
+  {
+    return redirect()->route('permissions.index');
+  }
+
+  public function update()
+  {
+    return redirect()->route('permissions.index');
+  }
+
+  public function destroy(Request $request, $id)
+  {
+    $permission = Permission::findOrFail($id);
+    if (Auth::user()->hasRole('Super-Admin')) {
+      $permission->delete();
+      $request->session()->flash('success', 'Permission deleted successfully');
+      return redirect()->route('permissions.index');
+    } else {
+      $request->session()->flash('error', 'You do not have permission to delete permission');
+      return redirect()->route('permissions.index');
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function create()
-    {
-        return redirect()->route('permissions.index');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function store(Request $request)
-    {
-
-        $this->validate($request, [
-            'name' => 'required|unique:permissions',
-        ]);
-
-        if (Auth::user()->hasRole('Super-Admin')) {
-            $permission = Permission::create(['name' => $request->input('name')]);
-            $permission->assignRole('Super-Admin');
-            $request->session()->flash('success', 'Permission created successfully');
-            return redirect()->route('permissions.index');
-        } else {
-            $request->session()->flash('error', 'You do not have permission to create permission');
-            return redirect()->route('permissions.index');
-        }
-        return redirect('/403');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\View\View
-     */
-    public function show($id)
-    {
-        return redirect()->route('permissions.index');
-    }
-
-    public function edit($id)
-    {
-        return redirect()->route('permissions.index');
-    }
-
-    public function update()
-    {
-        return redirect()->route('permissions.index');
-    }
-
-    public function destroy(Request $request, $id)
-    {
-        $permission = Permission::findOrFail($id);
-        if (Auth::user()->hasRole('Super-Admin')) {
-            $permission->delete();
-            $request->session()->flash('success', 'Permission deleted successfully');
-            return redirect()->route('permissions.index');
-        } else {
-            $request->session()->flash('error', 'You do not have permission to delete permission');
-            return redirect()->route('permissions.index');
-        }
-        return redirect('/403');
-    }
+    return redirect('/403');
+  }
 }
